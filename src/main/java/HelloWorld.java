3fcfdf5984951716bdb2254324f0fe791a5397ed
Samuel Jun <samuel.h.jun@gmail.com>
2013-04-29 23:08:51 -0700

    Another attempt to try to connect to database

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -5,7 +5,7 @@ import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.*;
 
 import java.sql.*;
-
+/*
 class ConnectionHolder implements HttpSessionBindingListener {
     private Connection con = null;
 
@@ -28,11 +28,11 @@ class ConnectionHolder implements HttpSessionBindingListener {
         // Do nothing when added to a Session
     }
 
-    public void valueUnbound(HttpSessionBindingEven event) {
+    public void valueUnbound(HttpSessionBindingEvent event) {
         // Roll back changes when removed from a Session
         // (or when the Session expires)
         try {
-            if (con!= null) {
+            if (con != null) {
                 con.rollback(); // abandon any uncomitted data
                 con.close();
             }
@@ -42,55 +42,35 @@ class ConnectionHolder implements HttpSessionBindingListener {
         }
     }
 }
+*/
+
+
 
 public class HelloWorld extends HttpServlet {
 
-    public void init() throws ServletException {
-        try {
-            Class.forName("oracle.jbdc.driver.OracleDriver");
-        }
-        catch (ClassNotFoundException e) {
-            System.out.println("Couldn't load OracleDriver");
-            throw new UnavailableException("Couldn't load OracleDriver");
-        }
+    private static Connection getConnection() throws URISyntaxException, SQLException {
+        URI dbUri = new URI(System.getenv("DATABASE_URL"));
+        
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = dbUri.getUserInfo().split(":")[1];
+        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + dbUri.getPath();
+        
+        return DriverManager.getConnection(dbUrl, username, password);
     }
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.getWriter().print("Hello from Java!\n");
-        response.setContentType("text/plain");
-        PrintWriter out = res.getWriter();
 
-        HttpSession session = request.getSession(true);
-        Connection con;
+        Connection connection = getConnection();
 
-        synchronized (session) {
-            ConnectionHolder holder = (ConnectionHolder) session.getAttribute("servletapp.connection");
-
-            if (holder == null) {
-                try {
-                    holder = new ConnectionHolder(DriverManager.getConnection("us-cdbr-east-03.cleardb.com","b5965fbb74b8d2","940be6f5"));
-                    session.setAttribute("servletapp.connection", holder);
-                }
-                catch (SQLException e) {
-                    log("Couldn't get db connection", e);
-                }
-            }
-
-            con = holder.getConnection();
-        }
-
-        try {
-            Statement stmt = con.createStatement();
-            stmt.execute("SHOW TABLES");
-        }
-        catch (Exception e) {
-            try {
-                con.rollback();
-                session.removeAttribute("servletapp.connection");
-            }
-            catch (Exception ignored) {
-            }
+        Statement stmt = connection.createStatement();
+        stmt.executeUpdate("DROP TABLE IF EXISTS ticks");
+        stmt.executeUpdate("CREATE TABLE ticks (tick timestamp)");
+        stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
+        ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
+        while (rs.next()) {
+            System.out.println("Read from DB: " + rs.getTimestamp("tick"));
         }
     }
 
