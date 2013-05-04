675befa8e59a057b08c755af777e0d3965e3b46a
Samuel Jun <samuel.h.jun@gmail.com>
2013-05-04 15:50:16 -0700

    Switching server from Jetty to Tomcat because of better understanding

---
+++ src/main/java/servlet/HelloServlet.java
@@ -0,0 +1,41 @@
+package servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.sql.*;
+
+@WebServlet(
+        name = "MyServlet",
+        urlPatterns = {"/hello"}
+    )
+public class HelloServlet extends HttpServlet {
+
+    // Database Connection
+    private static Connection getConnection() throws URISyntaxException, SQLException {
+        URI dbUri = new URI(System.getenv("DATABASE_URL"));
+
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = dbUri.getUserInfo().split(":")[1];
+        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + dbUri.getPath();
+
+        return DriverManager.getConnection(dbUrl, username, password);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ServletOutputStream out = resp.getOutputStream();
+        out.write("hello heroku".getBytes());
+        out.flush();
+        out.close();
+    }
+
+}
