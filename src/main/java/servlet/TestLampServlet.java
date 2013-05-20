b671e9596e99414c4518d3e904a92469613f1ecf
Samuel Jun <samuel.h.jun@gmail.com>
2013-05-19 18:32:05 -0700

    Fixing POST request bug where data_value is always 1

--- src/main/java/servlet/TestLampServlet.java
+++ src/main/java/servlet/TestLampServlet.java
@@ -78,7 +78,7 @@ public class TestLampServlet extends HttpServlet {
 
             // Insert latest test lamp change
             Statement stmt = connection.createStatement();
-            stmt.executeUpdate("INSERT INTO test_lamp VALUES (" + data_value_int + "', now())");
+            stmt.executeUpdate("INSERT INTO test_lamp VALUES (" + data_value_int + ", now())");
 
             // Return the latest status of the test lamp
             ResultSet rs = stmt.executeQuery("SELECT * FROM test_lamp ORDER BY time DESC LIMIT 1");
