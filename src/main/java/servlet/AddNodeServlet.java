80e8ea324453c4ed13703aade877191e625ed16f
sehunchoi <sehunchoi@ucla.edu>
2013-06-02 16:21:57 -0700

    addnode servlet test

--- src/main/java/servlet/AddNodeServlet.java
+++ src/main/java/servlet/AddNodeServlet.java
@@ -64,10 +64,10 @@ public class AddNodeServlet extends HttpServlet {
 
             Statement stmt = connection.createStatement();
             //INSERT INTO public.nodes ( node_id, address_low, address_high, hubs_hub_id, name, type ) VALUES ( ?, ?, ?, ?, ?, ? )
-            ResultSet rs = stmt.executeQuery("SELECT id FROM public.max_node_id");
-            rs.next();
-            int max_node_id = rs.getInt(1);
-            max_node_id++;
+            // ResultSet rs = stmt.executeQuery("SELECT id FROM public.max_node_id");
+            // rs.next();
+            int max_node_id = 2//rs.getInt(1);
+            // max_node_id++;
 
             stmt.executeQuery("INSERT INTO public.nodes ( node_id, address_low, address_high, hubs_hub_id, name, type ) VALUES (" 
                 + String.valueOf(max_node_id) + ", '" + input_address_low + "', '" + input_address_high +  "', " + 
