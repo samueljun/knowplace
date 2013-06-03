5b51b45ffe16a320cff0e5aa918a87126e05c7b0
Samuel Jun <samuel.h.jun@gmail.com>
2013-06-03 13:45:51 -0700

    Refactoring; Updated *.sql for new schema

--- src/main/java/servlet/DbManager.java
+++ src/main/java/servlet/DbManager.java
@@ -7,16 +7,18 @@ import java.net.URISyntaxException;
 import java.net.URI;
 
 public class DbManager {
-    public static Connection getConnection()
-    throws URISyntaxException, SQLException {
-        URI dbUri = new URI(System.getenv("DATABASE_URL"));
+	
+	private DbManager () {}
 
-        String username = dbUri.getUserInfo().split(":")[0];
-        String password = dbUri.getUserInfo().split(":")[1];
-        // Make sure the $DATABASE_URL environment variable is set
-        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + dbUri.getPath();
-        return DriverManager.getConnection(dbUrl, username, password);
-    }
+	public static Connection getConnection()
+	throws URISyntaxException, SQLException {
+		URI dbUri = new URI(System.getenv("DATABASE_URL"));
 
-    private DbManager () {}
+		String username = dbUri.getUserInfo().split(":")[0];
+		String password = dbUri.getUserInfo().split(":")[1];
+		// Make sure the $DATABASE_URL environment variable is set
+		String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + dbUri.getPath();
+		return DriverManager.getConnection(dbUrl, username, password);
+	}
+
 }
